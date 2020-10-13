e9ab66b5c68b73cfe27a3687f8d82ca3db56cf9d
stunggal <54833890+stunggal@users.noreply.github.com>
2020-10-13 13:49:10 +0700

    Add files via upload

---
+++ UAS/antian_rumah_sakit/src/antian_tukang_cukur/antrian_cukur.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antian_tukang_cukur;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author stunggal
+ */
+public class antrian_cukur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    ArrayList<String> nomor = new ArrayList<String>();
+    nomor.add("haslo");
+    }
+    
+    
+    
+}
